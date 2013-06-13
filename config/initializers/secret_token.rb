6e36e7fb67005aa4abdce31a584cab7f8c18a4a0
tihuinc@gmail.com <tihuinc@gmail.com>
2013-06-12 17:52:17 -0700

    First commit, rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwellBulletin::Application.config.secret_token = '40156834610a1519599533e4e2e7642a52f9590f08481c3091d84f8555a4f7581cf84edd837dd59b3c69b7e778858e8407ae72099651c35f74865ea56818d76a'
