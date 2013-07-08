5fd41e23bfcbb3bdb3587464967aef6d18559d4e
tihuinc@gmail.com <tihuinc@gmail.com>
2013-07-08 16:40:30 -0700

    Create Countries table

---
+++ db/migrate/20130708233924_create_countries_table.rb
@@ -0,0 +1,8 @@
+class CreateCountriesTable < ActiveRecord::Migration
+  def change
+    create_table :countries, :force => true do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613171813) do
+ActiveRecord::Schema.define(:version => 20130708233924) do
+
+  create_table "countries", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "surf_conditions", :force => true do |t|
     t.integer  "timestamp"
