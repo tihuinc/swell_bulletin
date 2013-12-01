9afa85bdf3d86b72226de6ab0bdda03512051bda
Leon Hu <tihuinc@gmail.com>
2013-12-01 15:18:26 -0800

    correct environment

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,6 +2,6 @@ role :web, "50.56.113.102"            # Your HTTP server, Apache/etc
 role :app, "50.56.113.102"            # This may be the same as your `Web` server
 role :db,  "50.56.113.102", :primary => true # This is where Rails migrations will run
 
-set :rails_env, "staging"
+set :rails_env, "stage"
 set :user, "ubuntu"
 set :password, "pass4Qual"
